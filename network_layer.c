93aa5b5a272e610cbd0306d2555aeaab31159d58
Ian Chen <StrikeC@users.noreply.github.com>
2017-12-26 10:06:25 -0600

    Add files via upload

--- network_layer.c
+++ network_layer.c
@@ -9,13 +9,14 @@
 #include <pthread.h>
 #include "structs.h"
 
+// handle error
 void error(const char *msg)
 {
     perror(msg);
     exit(0);
 }
 
-/*the thread function to receive packets from the data link layer and display the messages*/
+// the thread function to receive packets from the data link layer and display the messages
 void * rcvmsg (int threadsockfd)
 {
 	/*add codes for local varialbes*/
@@ -32,18 +33,39 @@ void * rcvmsg (int threadsockfd)
 
 int main(int argc, char *argv[])
 {
-	/*add codes for local variables*/
-	...
+	int sockfd, portno, ret;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+	char buffer[256];
 
-
-	/*check number of aruguments*/
+	// check number of aruguments
     if (argc < 4) {
        fprintf(stderr,"usage %s data_add data_port nickname\n", argv[0]);
        exit(0);
     }
 
 	/*add codes to connect to the data link layer*/
-    ...
+    // create socket
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket!");
+	// retrieve server name
+    server = gethostbyname(argv[1]);
+    if (server == NULL) 
+    {
+        fprintf(stderr,"ERROR, no such host!\n");
+        exit(0);
+    }
+	// fill in server address structure
+    bzero((char *)&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr_list[0], (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(portno);
+	// connect server
+    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+        error("ERROR connecting!");
+    printf("Ready to communicate\n");
 
 	/*creat a thread to receive packets from the data link layer*/
 	pthread_t pth;	// this is our thread identifier
@@ -53,7 +75,9 @@ int main(int argc, char *argv[])
 	while (1)
 	{
 		/*add codes to receive a message from keyboard, wrap it into a packet and send it to the data link layer*/
-		...
+		// receive message from keyboard
+        bzero(buffer, 256);
+        fgets(buffer, 255, stdin);
 
 		/*if the meassge is "EXIT"*/
 		if (strcmp (buffer, "EXIT\n")==0)
